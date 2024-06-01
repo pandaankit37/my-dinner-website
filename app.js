5054489457b6a7b406bae8e9dfece46c68c4ddd7
pandaankit37 <pandaankit37@gmail.com>
2024-06-01 22:52:05 +0530

    First Commit

--- app.js
+++ app.js
@@ -1,38 +1,34 @@
-function setMenu(itemMenu){
-    window.alert("Anda memilih " + itemMenu);
-    document.getElementById('select-menu').value = itemMenu;
-}
-
-gsap.from("#titleHead", {
-    opacity: 0, 
-    y: -50, 
-    x: 0,
-    duration: 2
-});
-gsap.from("#desc", {
-    opacity: 0, 
-    y: 50, 
-    x: 0,
-    duration: 2
-});
-gsap.from(".btn", {
-    opacity: 0, 
-    y: 50, 
-    x: 0,
-    duration: 2
-});
-
-
-if (localStorage.getItem('theme')=='light') {
-    setTheme(true);
-}
-
-function setTheme(isLight) {
-    if(isLight) {
-        document.body.setAttribute('id', 'lightMode');
-        localStorage.setItem('theme', 'light');
-    }else{
-        document.body.setAttribute('id', '');
-        localStorage.removeItem('theme');
+$(document).ready(function () {
+    function setMenu(itemMenu) {
+        alert("Anda memilih " + itemMenu);
+        $('#select-menu').val(itemMenu);
     }
-}
+
+    gsap.from("#titleHead, #desc, .btn", {
+        opacity: 0,
+        y: function(index, target) {
+            return index === 0 ? -50 : 50;
+        },
+        x: 0,
+        duration: 2
+    });
+
+    if (localStorage.getItem('theme') === 'light') {
+        setTheme(true);
+    }
+
+    function setTheme(isLight) {
+        $('body').attr('id', isLight ? 'lightMode' : '');
+        if (isLight) {
+            localStorage.setItem('theme', 'light');
+        } else {
+            localStorage.removeItem('theme');
+        }
+    }
+
+    $(function () {
+        $('.additional-btn').click(function () {
+            alert('Additional button clicked!');
+        });
+    });
+});
